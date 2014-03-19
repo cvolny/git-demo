d3f14aab3935e237ea85a8f0421131340f639d86
Christopher J. Volny <cvolny@gmail.com>
2014-03-19 14:54:43 -0400

    Added factorial program.

---
+++ myprogram.py
@@ -0,0 +1,32 @@
+"""
+ A sample program!
+
+ ~cvolny
+"""
+
+
+def factorial(x):
+    """
+    Calculate the factorial of x iteratively.
+    """
+    y = 1
+    if not float(x).is_integer():
+        raise ValueError("Factorial undefined for non-integers.")
+    if x < 1:
+        raise ValueError("Factorial undefined for less than 1.")
+    while x > 1:
+        y *= x
+        x -= 1
+    return y
+
+
+if "__main__" == __name__:
+    while True:
+        try:
+            m = raw_input("Enter an integer: ")
+            d = int(m)
+            r = factorial(d)
+            print "{0:d}! = {1:d}".format(d, r)
+            break
+        except Exception:
+            print "Invalid input, try again."
